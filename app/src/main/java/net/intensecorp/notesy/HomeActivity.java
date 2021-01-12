ae0ec9995fe2f72bd9c347c7049403ee30976742
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-01-12 10:57:40 +0530

    feat(activity): add button code to launch CreateNoteActivity

--- app/src/main/java/net/intensecorp/notesy/HomeActivity.java
+++ app/src/main/java/net/intensecorp/notesy/HomeActivity.java
@@ -1,14 +1,25 @@
 package net.intensecorp.notesy;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class HomeActivity extends AppCompatActivity {
+    private static final int REQUEST_CODE_ADD_NOTE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ImageView addNoteButton = findViewById(R.id.imageView_add_note);
+        addNoteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivityForResult(new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE);
+            }
+        });
     }
 }
