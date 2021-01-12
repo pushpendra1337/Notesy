e667415e70663dd88f823973cfb5651c5945f005
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-01-12 10:52:43 +0530

    feat(activity): create activity for adding new notes
    Created CreateNoteActivity.java, activity_create_note.xml and added them to AndroidManifest.xml

---
+++ app/src/main/java/net/intensecorp/notesy/CreateNoteActivity.java
@@ -0,0 +1,24 @@
+package net.intensecorp.notesy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class CreateNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_note);
+        ImageView backButton = findViewById(R.id.imageView_back);
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+    }
+}
