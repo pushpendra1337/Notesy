ecb9f452b0ff82eb8458c0b9c828e80f000acfba
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-01-13 10:34:00 +0530

    fix(dao): add @Dao annotation to class NoteDao

--- app/src/main/java/net/intensecorp/notesy/dao/NoteDao.java
+++ app/src/main/java/net/intensecorp/notesy/dao/NoteDao.java
@@ -1,5 +1,6 @@
 package net.intensecorp.notesy.dao;
 
+import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -9,6 +10,7 @@ import net.intensecorp.notesy.entities.Note;
 
 import java.util.List;
 
+@Dao
 public interface NoteDao {
 
     @Query("SELECT * FROM notes ORDER BY id DESC")
