fd66c604eb9ad37990015e4d5af4d6b81268c060
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-01-13 09:16:20 +0530

    feat(database): create note entity class inside entities package

---
+++ app/src/main/java/net/intensecorp/notesy/entities/Note.java
@@ -0,0 +1,105 @@
+package net.intensecorp.notesy.entities;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "notes")
+public class Note implements Serializable {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "title")
+    private String title;
+
+    @ColumnInfo(name = "timestamp")
+    private String timestamp;
+
+    @ColumnInfo(name = "subtitle")
+    private String subtitle;
+
+    @ColumnInfo(name = "note_content")
+    private String noteContent;
+
+    @ColumnInfo(name = "image_path")
+    private String imagePath;
+
+    @ColumnInfo(name = "note_color")
+    private String noteColor;
+
+    @ColumnInfo(name = "web_link")
+    private String webLink;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public String getSubtitle() {
+        return subtitle;
+    }
+
+    public void setSubtitle(String subtitle) {
+        this.subtitle = subtitle;
+    }
+
+    public String getNoteContent() {
+        return noteContent;
+    }
+
+    public void setNoteContent(String noteContent) {
+        this.noteContent = noteContent;
+    }
+
+    public String getImagePath() {
+        return imagePath;
+    }
+
+    public void setImagePath(String imagePath) {
+        this.imagePath = imagePath;
+    }
+
+    public String getNoteColor() {
+        return noteColor;
+    }
+
+    public void setNoteColor(String noteColor) {
+        this.noteColor = noteColor;
+    }
+
+    public String getWebLink() {
+        return webLink;
+    }
+
+    public void setWebLink(String webLink) {
+        this.webLink = webLink;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return title + " : " + timestamp;
+    }
+}
