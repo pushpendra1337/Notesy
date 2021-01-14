1a0eca3f8934912cdea2fc88eb407e438a9df5a4
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-01-14 09:02:25 +0530

    feat(getNotes): add method getNotes() to retrieve notes in logcat only

--- app/src/main/java/net/intensecorp/notesy/activities/HomeActivity.java
+++ app/src/main/java/net/intensecorp/notesy/activities/HomeActivity.java
@@ -1,14 +1,20 @@
 package net.intensecorp.notesy.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import net.intensecorp.notesy.R;
-import net.intensecorp.notesy.activities.CreateNoteActivity;
+import net.intensecorp.notesy.database.NotesDatabase;
+import net.intensecorp.notesy.entities.Note;
+
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_ADD_NOTE = 1;
@@ -24,5 +30,24 @@ public class HomeActivity extends AppCompatActivity {
                 startActivityForResult(new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE);
             }
         });
+
+        getNotes();
+    }
+
+    private void getNotes() {
+        @SuppressLint("StaticFieldLeak")
+        class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
+            @Override
+            protected List<Note> doInBackground(Void... voids) {
+                return NotesDatabase.getDatabase(getApplicationContext()).noteDao().getAllNotes();
+            }
+
+            @Override
+            protected void onPostExecute(List<Note> notes) {
+                super.onPostExecute(notes);
+                Log.d("MY_NOTES", notes.toString());
+            }
+        }
+        new GetNotesTask().execute();
     }
 }
