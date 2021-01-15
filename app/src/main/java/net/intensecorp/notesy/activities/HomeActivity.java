764c94132bb7406614331e418459c2719e770fa2
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-01-15 14:12:32 +0530

    feat(note): add code to display note

--- app/src/main/java/net/intensecorp/notesy/activities/HomeActivity.java
+++ app/src/main/java/net/intensecorp/notesy/activities/HomeActivity.java
@@ -4,20 +4,27 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.StaggeredGridLayoutManager;
 
 import net.intensecorp.notesy.R;
+import net.intensecorp.notesy.adapters.NotesAdapter;
 import net.intensecorp.notesy.database.NotesDatabase;
 import net.intensecorp.notesy.entities.Note;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
     private static final int REQUEST_CODE_ADD_NOTE = 1;
+    private RecyclerView mNotesRecyclerView;
+    private List<Note> mNoteList;
+    private NotesAdapter mNotesAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +37,13 @@ public class HomeActivity extends AppCompatActivity {
                 startActivityForResult(new Intent(getApplicationContext(), CreateNoteActivity.class), REQUEST_CODE_ADD_NOTE);
             }
         });
+
+        mNotesRecyclerView = findViewById(R.id.recyclerView_notes);
+        mNotesRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
+
+        mNoteList = new ArrayList<>();
+        mNotesAdapter = new NotesAdapter(mNoteList);
+        mNotesRecyclerView.setAdapter(mNotesAdapter);
 
         getNotes();
     }
@@ -45,9 +59,24 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                Log.d("MY_NOTES", notes.toString());
+                if (mNoteList.size() == 0) {
+                    mNoteList.addAll(notes);
+                    mNotesAdapter.notifyDataSetChanged();
+                } else {
+                    mNoteList.add(0, notes.get(0));
+                    mNotesAdapter.notifyItemInserted(0);
+                }
+                mNotesRecyclerView.smoothScrollToPosition(0);
             }
         }
         new GetNotesTask().execute();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
+            getNotes();
+        }
     }
 }
