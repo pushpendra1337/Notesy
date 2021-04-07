d3d307da045e57748c022e0a5b96a27659abc384
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-04-07 10:59:09 +0530

    feat(viewOrUpdateNote): implement NotesListener to listen for view action

--- app/src/main/java/net/intensecorp/notesy/activities/HomeActivity.java
+++ app/src/main/java/net/intensecorp/notesy/activities/HomeActivity.java
@@ -16,15 +16,20 @@ import net.intensecorp.notesy.R;
 import net.intensecorp.notesy.adapters.NotesAdapter;
 import net.intensecorp.notesy.database.NotesDatabase;
 import net.intensecorp.notesy.entities.Note;
+import net.intensecorp.notesy.listeners.NotesListener;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NotesListener {
     private static final int REQUEST_CODE_ADD_NOTE = 1;
+    private static final int REQUEST_CODE_UPDATE_NOTE = 2;
+    private static final int REQUEST_CODE_SHOW_NOTES = 3;
+
     private RecyclerView mNotesRecyclerView;
     private List<Note> mNoteList;
     private NotesAdapter mNotesAdapter;
+    private int mNoteClickedPosition = -1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +47,13 @@ public class HomeActivity extends AppCompatActivity {
         mNotesRecyclerView.setLayoutManager(new StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL));
 
         mNoteList = new ArrayList<>();
-        mNotesAdapter = new NotesAdapter(mNoteList);
+        mNotesAdapter = new NotesAdapter(mNoteList, this);
         mNotesRecyclerView.setAdapter(mNotesAdapter);
 
-        getNotes();
+        getNotes(REQUEST_CODE_SHOW_NOTES);
     }
 
-    private void getNotes() {
+    private void getNotes(final int requestCode) {
         @SuppressLint("StaticFieldLeak")
         class GetNotesTask extends AsyncTask<Void, Void, List<Note>> {
             @Override
@@ -59,14 +64,18 @@ public class HomeActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(List<Note> notes) {
                 super.onPostExecute(notes);
-                if (mNoteList.size() == 0) {
+                if (requestCode == REQUEST_CODE_SHOW_NOTES) {
                     mNoteList.addAll(notes);
                     mNotesAdapter.notifyDataSetChanged();
-                } else {
+                } else if (requestCode == REQUEST_CODE_ADD_NOTE) {
                     mNoteList.add(0, notes.get(0));
                     mNotesAdapter.notifyItemInserted(0);
+                    mNotesRecyclerView.smoothScrollToPosition(0);
+                } else if (requestCode == REQUEST_CODE_UPDATE_NOTE) {
+                    mNoteList.remove(mNoteClickedPosition);
+                    mNoteList.add(mNoteClickedPosition, notes.get(mNoteClickedPosition));
+                    mNotesAdapter.notifyItemChanged(mNoteClickedPosition);
                 }
-                mNotesRecyclerView.smoothScrollToPosition(0);
             }
         }
         new GetNotesTask().execute();
@@ -76,7 +85,20 @@ public class HomeActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == REQUEST_CODE_ADD_NOTE && resultCode == RESULT_OK) {
-            getNotes();
+            getNotes(REQUEST_CODE_ADD_NOTE);
+        } else if (requestCode == REQUEST_CODE_UPDATE_NOTE && resultCode == RESULT_OK) {
+            if (data != null) {
+                getNotes(REQUEST_CODE_UPDATE_NOTE);
+            }
         }
+    }
+
+    @Override
+    public void onNoteClicked(Note note, int position) {
+        mNoteClickedPosition = position;
+        Intent intent = new Intent(getApplicationContext(), CreateNoteActivity.class);
+        intent.putExtra("isViewOrUpdate", true);
+        intent.putExtra("note", note);
+        startActivityForResult(intent, REQUEST_CODE_UPDATE_NOTE);
     }
 }
