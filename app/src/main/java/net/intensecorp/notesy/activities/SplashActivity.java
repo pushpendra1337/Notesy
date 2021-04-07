eec86c59d7fd5486496285ea07fd7e51f440f614
Pushpendra Yadav <pushpendray1337@gmail.com>
2021-04-07 11:00:25 +0530

    refactor(SplashActivity.java): rearrange code

--- app/src/main/java/net/intensecorp/notesy/activities/SplashActivity.java
+++ app/src/main/java/net/intensecorp/notesy/activities/SplashActivity.java
@@ -1,13 +1,12 @@
 package net.intensecorp.notesy.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import net.intensecorp.notesy.R;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
